850ad9ba704d15b0d138a0368f3fc32e403acc11
AayushJadhav <aayush.jadhav117@gmail.com>
2023-04-10 19:49:41 +0530

    Setup done.

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
+const gameMenu = document.querySelector("#game-menu")
+const gameArea = document.querySelector("#game-area")
+const endgameArea = document.querySelector("#endgame-area")
 const gridDiv = document.querySelector("#grid")
+const startBtn = document.querySelector(".start-btn")
+const turnH3 = document.querySelector("#currentPlayer")
+const resultH3 = document.querySelectorAll("#result")
 
 const gridArray = []
 
-createGrid()
+var currentPlayer = 1
+turnH3.innerHTML = currentPlayer
+
+startBtn.addEventListener('click', startGame)
+
+function startGame() {
+    gameMenu.style.display = "none"
+    gameArea.style.display = "flex"
+    createGrid()
+}
 
 function createGrid() {
-    for (var y = 0; y <= 6; y++) {
-        for (var x = 0; x <= 6; x++) {
+    for (let y = 0; y <= 19; y++) {
+        for (let x = 0; x <= 19; x++) {
             var square = document.createElement('div')
             gridArray.push([square, x, y])
-            if (y == 6) {
-                square.classList.add("taken")
-            }
             gridDiv.appendChild(square)
         }
     }
 
-    console.log(gridArray)
+    for (let i = 0; i < gridArray.length; i++) {
+        gridArray[i].push(i)
+        gridArray[i][0].id = i
+    }
 }
